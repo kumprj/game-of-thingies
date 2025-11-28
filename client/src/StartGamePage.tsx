ca11c38d4352f979f06fc8d76cbc0a21fa9ce0af
Robert Kump <rkump24@gmail.com>
2025-11-27 19:42:12 -0500

    updates

--- client/src/StartGamePage.tsx
+++ client/src/StartGamePage.tsx
@@ -4,7 +4,7 @@ import {useParams} from "react-router-dom";
 import logo from "../src/assets/logo.jpg";
 
 // Set backend API base URL
-axios.defaults.baseURL = "https://vypjravvki.execute-api.us-east-1.amazonaws.com/dev";
+axios.defaults.baseURL = "https://i7v5llgsek.execute-api.us-east-1.amazonaws.com/dev";
 
 interface Entry {
   entryId: string;
